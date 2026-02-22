ef11c33d8b2091c8e86454bddd41eba3c7fb59fe
cofeina <dubaj.andrzej@gmail.com>
2026-02-22 13:49:14 -0500

    feat: fix formatting

---
+++ target/debug/deps/ch01_hello_world-c3896e8b84342fd5.d
@@ -0,0 +1,6 @@
+/home/cofeina/Programming/rust-learning/target/debug/deps/ch01_hello_world-c3896e8b84342fd5.d: rust-book/ch01-hello-world/src/main.rs rust-book/ch01-hello-world/src/tests.rs
+
+/home/cofeina/Programming/rust-learning/target/debug/deps/ch01_hello_world-c3896e8b84342fd5: rust-book/ch01-hello-world/src/main.rs rust-book/ch01-hello-world/src/tests.rs
+
+rust-book/ch01-hello-world/src/main.rs:
+rust-book/ch01-hello-world/src/tests.rs:
